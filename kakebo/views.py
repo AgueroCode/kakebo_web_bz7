4009a076123c5992ceb57d364d5afed2181e69b0
AgueroCode <gomezdeaguero@gmail.com>
2021-05-27 12:32:37 +0200

    Ahora podemos insertar regiestros a la BD

--- kakebo/views.py
+++ kakebo/views.py
@@ -1,6 +1,6 @@
 from kakebo import app
 import sqlite3
-from flask import jsonify, render_template, request  #jsonify hace algo parecido al json.dumps
+from flask import jsonify, render_template, request, redirect, url_for  #jsonify hace algo parecido al json.dumps
 from kakebo.forms import MovimientosForm
 
 @app.route('/')
@@ -37,8 +37,31 @@ def nuevo():
         return render_template('alta.html', form = formulario)
     else:
         if formulario.validate(): #comprueba si se cumplen los validadores que pusimos en los metodos metidos en forms.py Si hay errores nos los informa.
-            pass
-            #Insertar movimiento en la base de datos
-            #Redirect a la ruta /
+            conexion = sqlite3.connect("movimientos.db")
+            cur = conexion.cursor()
+
+            query = "INSERT INTO movimientos (fecha, concepto, categoria, esGasto, cantidad) VALUES (?, ?, ?, ?, ?)"
+            try:
+                cur.execute(query, [formulario.fecha.data, formulario.concepto.data, formulario.categoria.data,
+                                formulario.esGasto.data, formulario.cantidad.data])
+            except sqlite3.Error as el_error:
+                print("Error en SQL INSERT", el_error)
+                return  render_template('alta.html', form=formulario)
+            
+            # query = """
+            #     INSERT INTO movimientos (fecha, concepto, categoria, esGasto, cantidad) VALUES
+            #     (:fecha, :concepto, :categoria, :esgasto, :cantidad)
+            # """
+            # cur.execute(query, {
+            #                     'fecha': formulario.fecha.data, 
+            #                     'concepto': formulario.concepto.data, 
+            #                     'categoria': formulario.categoria.data,
+            #                     'esGasto': formulario.esGasto.data, 
+            #                     'cantidad': formulario.cantidad.data])
+
+            conexion.commit() #esto fija el cambio en la base de datos, confirma el cambio
+            conexion.close()
+
+            return redirect(url_for("index")) #Redirect a la ruta /. podria hacerse poniendo simplemente "/" usamos url_for para evitar problemas si cambia la direccion de la ruta
         else:
             return render_template('alta.html', form = formulario) #esto tiene sentido porque el validate nos va a informar tambien los errores
